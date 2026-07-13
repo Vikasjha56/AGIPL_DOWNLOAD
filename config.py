0cbf6bd9e3df37200a653f5b2eedca3b911a71e9
Vikasjha56 <97323544+Vikasjha56@users.noreply.github.com>
2026-07-14 01:33:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+GOOGLE_SHEETS = [
+
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTvOh0XRODnQA2X_hMY9yjzJYA1PcveXN7Vuzpi3oNLcP3rQayHE9Oz_acQm9MaD6l68_QIjKh9NcxW/pub?output=csv",
+
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRiHISzx9mRbmNe-xD52whwaXP-gssSFgvWFO33MI9lzrkPXZZk9YjIULYoTkS1QODsJP4jlQIcqF5Q/pub?output=csv",
+
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRWyYVk3l9rCkLSJIA0OP-9tKR1yXQfqr7gbH3UImo7gobxHQFktgtlnzk_OAs75fWwALVkh1f28BN/pub?output=csv",
+
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vT9u26fnfjIU2WioFzjQau5zStkYhfv4Z5610KV9402vocvu0aOlusIGN89HBjQDcKXd1tmQvOczNhC/pub?output=csv",
+
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vStmLhR5LIJAj2q4arh91ngQQc4G29RXw4ysueuHML_QUwtZDjcjAQoQAj1DEgMXhjmOUt99CJXjDz1/pub?output=csv"
+
+]
