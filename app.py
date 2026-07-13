991b7453f2ccaa6112678e46e37f3be5b98397fe
Vikasjha56 <97323544+Vikasjha56@users.noreply.github.com>
2026-07-14 01:17:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, send_file
+from google_reader import get_master_table
+from excel_export import create_excel
+from pdf_export import create_pdf
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+
+    return "AGIPL Download System is Running Successfully."
+
+
+
+@app.route("/download_excel")
+def download_excel():
+
+    try:
+
+        print("Creating Master Table for Excel...")
+
+        master = get_master_table()
+
+        print("Generating Excel...")
+
+        file_path = create_excel(master)
+
+
+        return send_file(
+            file_path,
+            as_attachment=True
+        )
+
+
+    except Exception as e:
+
+        return f"Excel Error : {str(e)}"
+
+
+
+
+@app.route("/download_pdf")
+def download_pdf():
+
+    try:
+
+        print("Creating Master Table for PDF...")
+
+        master = get_master_table()
+
+        print("Generating PDF...")
+
+        file_path = create_pdf(master)
+
+
+        return send_file(
+            file_path,
+            as_attachment=True
+        )
+
+
+    except Exception as e:
+
+        return f"PDF Error : {str(e)}"
+
+
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
