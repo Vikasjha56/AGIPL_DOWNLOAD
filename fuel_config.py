8a8cd7bd52191022cfd08c6cdd821b16507d952b
Vikasjha56 <97323544+Vikasjha56@users.noreply.github.com>
2026-07-17 14:56:23 +0530

    Update fuel_config.py

--- fuel_config.py
+++ fuel_config.py
@@ -1,1 +1,13 @@
+# =====================================================
+# AGIPL FUEL INTELLIGENCE CENTER CONFIG
+# =====================================================
 
+
+FUEL_GOOGLE_SHEET = (
+    "https://docs.google.com/spreadsheets/d/e/"
+    "2PACX-1vT3e08cKMWasrA0FhI8Z8lAhSwgswvOoyYijyqGbHqetAUk-ga0LP3NuoCcVyMp7A/"
+    "pub?output=csv"
+)
+
+
+FUEL_SHEET_NAME = "Machine Working Report"
